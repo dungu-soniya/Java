c22fde18ae9e6982d6588891adf4ec0cf9a6d5ae
dungu-soniya <dungusoniya.25.csm@anits.edu.in>
2026-09-20 17:04:51 +0530

    Create TryDemo.java

---
+++ Practice programs/TryDemo.java
@@ -0,0 +1,10 @@
+public class TryDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 2;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        }
+    }
+}
